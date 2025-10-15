98df166b7edee728758e31472c94f92125595b04
valinor <ajgarnik@gmail.com>
2025-10-15 19:31:23 +0300

    improve version calculation - get a tag closest to HEAD, select newest one if there are multiple tags on the commit

--- src/main/kotlin/net/agl/gradle/VersionFromGit.kt
+++ src/main/kotlin/net/agl/gradle/VersionFromGit.kt
@@ -9,12 +9,14 @@ import org.eclipse.jgit.lib.Constants
 import org.eclipse.jgit.lib.Constants.R_TAGS
 import org.eclipse.jgit.lib.IndexDiff
 import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.revwalk.RevTag
 import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.treewalk.FileTreeIterator
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.security.DigestOutputStream
 import java.security.MessageDigest
+import kotlin.use
 
 // ANSI colors
 private const val RESET: String = "\u001b[0m"
@@ -62,6 +64,35 @@ private fun dirtyHash(repo: Repository): String {
     return digest.digest().joinToString("") { "%02x".format(it) }
 }
 
+fun getLastTag(repo: Repository): Pair<RevTag?, Int?> {
+    RevWalk(repo).use { walk ->
+        val tags = repo.refDatabase.getRefsByPrefix(R_TAGS)
+            .map { walk.parseAny(it.objectId) }
+            // Filter out non-annotated tags (which are represented as RevCommit)
+            .filterIsInstance<RevTag>()
+            // Group by commit hash
+            .groupBy { tag -> tag.`object`.name }
+
+        // No tags found
+        if (tags.isEmpty()) {
+            return Pair(null, null)
+        }
+
+        // Walk from the HEAD commit to the first tagged commit, to find the last tag
+        walk.markStart(walk.parseCommit(repo.resolve(Constants.HEAD)))
+        var ncommits = 0
+        for (commit in walk) {
+            if (!tags.containsKey(commit.name)) {
+                ncommits++
+            } else {
+                // Take the chronologically last tag found for the commit
+                return Pair(tags[commit.name]!!.maxBy { tag -> tag.taggerIdent.whenAsInstant }, ncommits)
+            }
+        }
+    }
+    return Pair(null, null)
+}
+
 fun versionFromGit(
     gitRoot: String,
     dirtyDetect: Boolean = true,
@@ -72,39 +103,27 @@ fun versionFromGit(
     try {
         Git.open(File(gitRoot)).use { git ->
             val repo = git.repository
-            val walk = RevWalk(repo)
 
-            val lastTag = repo.refDatabase.getRefsByPrefix(R_TAGS)
-                .map { repo.refDatabase.peel(it) }
-                .filter { it.peeledObjectId != null }
-                .takeIf { it.isNotEmpty() }
-                ?.last()
+            val (lastTag, ncommits) = getLastTag(repo)
             if (lastTag == null) {
                 System.err.println("${RED}No tags found, falling back to default version ${GREEN}${fallbackVersion}${RESET}")
                 return fallbackVersion
             }
-
-            val version = StringBuilder(lastTag.name.substring(R_TAGS.length))
+            val version = StringBuilder(lastTag.tagName)
             if (version.endsWith("-SNAPSHOT")) {
                 System.err.println("${YELLOW}SNAPSHOT version found. Skipping further calculations${RESET}")
                 println("Version calculated: ${GREEN}$version${RESET}")
                 return version.toString()
             }
 
-            if (commitsNo) {
-                val lastTagObj = lastTag.peeledObjectId ?: lastTag.objectId
-                val lastTagCommit = walk.parseCommit(lastTagObj)
-                val headId = repo.resolve("HEAD")
-                val headCommit = walk.parseCommit(headId)
-                walk.reset()
-                walk.markStart(headCommit)
-                walk.markUninteresting(lastTagCommit)
-                val tagToHeadCommits = walk.count()
-                val headHash = headId.name.substring(0, 7)
-
-                if (tagToHeadCommits > 0) {
-                    version.append('-').append(tagToHeadCommits).append("-g").append(headHash)
-                }
+            ncommits?.takeIf { commitsNo && it > 0 }?.let {
+                version.append('-')
+                    .append(it)
+                    .append("-g")
+                    .append(
+                        repo.refDatabase.exactRef("HEAD")
+                            .objectId.name.substring(0, 7)
+                    )
             }
 
             if (dirtyDetect) {
@@ -120,7 +139,7 @@ fun versionFromGit(
             println("Version calculated: ${GREEN}$version${RESET}")
             return version.toString()
         }
-    } catch (e: RepositoryNotFoundException) {
+    } catch (_: RepositoryNotFoundException) {
         System.err.println("${RED}No GIT repository found in [${gitRoot}], falling back to default version ${GREEN}${fallbackVersion}${RESET}")
         return fallbackVersion
     }
